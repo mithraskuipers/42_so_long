f9434cfbbb5b167d93a7d2fbfdc663869c14d343
Mithras Kuipers <mikuiper@f1r3s17.codam.nl>
2022-01-24 11:11:09 +0100

    update

--- src/main.c
+++ src/main.c
@@ -36,40 +36,6 @@ int	open_file(char *filepath)
 	return (fd);
 }
 
-/*
-int	ft_nlines_fd(char *filepath)
-{
-	int	nlines;
-	int	fd;
-	fd = open(filepath, O_RDONLY);
-	if (fd < 0)
-	{
-		ft_exit_failure("Please provide a valid map.");
-		return (EXIT_FAIL);
-	}
-	nlines = 0;
-}
-*/
-
-
-//map_width = 
-
-/*
-{
-	//map->width = ft_strlen_char(filepath, '\n');
-	//map->width = ft_strlen_char(filepath, '\n'); // this is not good. you want not the file path, but contents of the file
-	
-	//while (file[root->game->width] && file[root->game->width] != '\n')
-	//	root->game->width++;
-	//if (root->game->width == 0 || file[root->game->width] == 0)
-	//{
-	//	free(file);
-	//	root_destroy(root, "map file is invalid", 0);
-	//}
-
-}
-*/
-
 int		map_width(int fd)
 {
 	char	buff[1];
@@ -89,37 +55,6 @@ int		map_width(int fd)
 	}
 	return (len);
 }
-
-/*
-int	ft_count_lines(int fd, int x, int img_w)
-//returns how many lines the file of fd contains
-{
-	char	buffer[1];
-	int		linecount;
-	int		bytes;
-	int		i;
-
-	i = 0;
-	buffer[0] = '\0';
-	linecount = 1;
-	bytes = 1;
-	while (bytes == 1)
-	{
-		bytes = read(fd, buffer, 1);
-		if (bytes != 1 && i != (x / img_w))
-			exit_error();
-		if (i == (x / img_w))
-		{
-			linecount++;
-			i = 0;
-		}
-		else
-			i++;
-	}
-	return (linecount);
-}
-*/
-
 
 int		map_height(int fd, int map_width)
 {
@@ -177,6 +112,7 @@ int		main(int argc, char **argv)
 		ft_exit_failure("Memory allocation issue.");
 	check_input(argc, argv);
 	map_parser(map, argv[1]);
+	mlx_init();
 	//printf("%d", map->width);
 	//printf("%s", ft_strchr(argv[1], 1));
 
